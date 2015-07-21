6835e9b26905cc155de4feb214d42d12f9f05b6e
denzyl <denzyl@live.nl>
2015-07-21 08:33:06 +0200

    Removed foreach resultset

--- app/bin/Model.php
+++ app/bin/Model.php
@@ -27,19 +27,10 @@ abstract class Model
     public static function find(array $clause,$limit = null)
     {
         $name = get_called_class();
-
         $query = new Builder();
         $resultSet = $query->select()->from($name)->where($clause)->limit($limit)->execute();
         echo($resultSet);
-        $result = new ResultSet();
-        foreach ($resultSet as $key => $set ) {
-            $set = new Set();
-            $set->$key = $set;
-            $result->add($set);
         }
-        return $resultSet;
-
-    }
 
     public function save()
     {
