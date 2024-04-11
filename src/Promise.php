0fcc58e9458fb35bbd0839af1b168fb21b0c9cb8
Denzyl Dick <2477646+denzyldick@users.noreply.github.com>
2024-04-12 00:56:52 +0200

    Update Promise.php

--- src/Promise.php
+++ src/Promise.php
@@ -2,6 +2,8 @@
 
 namespace Av;
 
+use Av\Exception\NotHandelingError;
+
 class Promise {
 
   public static function exec(Promisable $promisiable): Promise {
@@ -9,17 +11,16 @@ class Promise {
     $promisiable->setPromise($promisiable);
     return $promisiable;
   }
-
-
   public function setPromise(Promisable $promisable) {
     $this->promisiable = $promisable;
   }
   public function then(callable $ok, callable $fail) {
+    if(is_callable($fail) === false){
+       throw new NotHandlingError();
+    }
     if (is_callable($ok) && is_callable($fail)) {
-
       try {
         $result = $this->wait();
-
         $ok($result);
       } catch (\Throwable $e) {
         $fail($e);
