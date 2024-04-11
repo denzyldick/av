57202da2b64e713bce3a1c80287cfcc016cb346b
Denzyl Dick <ddenzyl@gmail.com>
2024-04-12 01:06:03 +0200

    test

--- src/Promise.php
+++ src/Promise.php
@@ -1,11 +1,13 @@
+
 <?php
 
 namespace Av;
 
 use Av\Exception\NotHandelingError;
 
-class Promise {
 
+class Promise {
+  private bool $ran = false;
   public static function exec(Promisable $promisiable): Promise {
     $promisiable = self();
     $promisiable->setPromise($promisiable);
@@ -15,22 +17,35 @@ class Promise {
     $this->promisiable = $promisable;
   }
   public function then(callable $ok, callable $fail) {
-    if(is_callable($fail) === false){
-       throw new NotHandlingError();
+    if (is_callable($fail) === false) {
+      throw new NotHandlingError();
     }
     if (is_callable($ok) && is_callable($fail)) {
       try {
         $result = $this->wait();
         $ok($result);
+        $this->ran = true;
       } catch (\Throwable $e) {
         $fail($e);
+        if (get_class() === 'Exception') {
+          throw new $e;
+        }
       }
     }
   }
 
-  public function wait($wait = false) {
-    if ($wait === false) {
-      return $this->run();
+  public function wait() {
+    if ($this->ran) {
+      throw new \Av\Exception\PromiseAlreadySuccessfullyRan();
     }
+    return $this->then(
+      function ($result) {
+        return $result;
+      },
+      function (Throwable $e) {
+
+        echo $e->getMessage();
+      }
+    );
   }
 }
